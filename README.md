34cd5bcb84fd08f81c5b8ea6aaaec88b658639b0
Yeongjin Kim <7591blitzloft@gmail.com>
2024-11-19 00:16:59 +0900

    feat: Middleware set and is ready to fetch data from the backend; README updated

--- README.md
+++ README.md
@@ -2,17 +2,37 @@
 
 Frontend built for BusiDiag.
 
+[BusiDiag](https://busidiag.github.io/front/)
+
 ## Pages
 
-### Businesses
+### Business
 
-This page is for managing all the businesses added to the app. Adding and deleting business is available.
+This page is for managing all the businesses added to the app. The page as it is requested will send its request to DB to bring all the basic information of each businesses, and each business will show up with options including detail view and delete.
 
-### Results
+#### BusinessNew
 
-This is where all the results are shown and managed. User will be able to select one of the business (if one was selected before in the Businesses section, the one selected will show up), and selecting one wanted will show all the results made beforehand.
+This page is for adding new business, which is accessible via the Business page. A form will bring up, and the user has to type in the characteristics of the business. Submitting the form would send the request to the DB, resulting in inserting new business in the DB ready to be diagnosed.
+
+### Diagnosis
+
+This page to start new diagnosis for certain business of user's choice. The whole list of businesses in the DB will appear in a select box, and the user will choose one of it which will bring them to the actual diagnosis page(DiagnosisForm).
+
+#### DiagnosisForm
+
+This page is where the actual diagnosis is held in. As the page loads, this page will send a request to the backend. The backend will select questions that will meet the user's needs from the DB, and the user will, in a result, see a pile of questions to answer to in a 1-5 score. After answering all the questions, the user can save the form which will bring that user to the result page of the chosen business.
+
+### Result
+
+This page is where the results of diagnosis for each business will be shown at. The user can see the scores of diagnosises done beforehand, and compare them with other diagnosises done in other times.
 
 ## Stacks
 
 - React
 - Tailwind CSS
+- gh-pages
+
+## TODO
+
+- Connect to the backend
+- Change DiagnosisForm's actions - to bring questions from the DB, not the JSON file
--- src/pages/Business.js
+++ src/pages/Business.js
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
+
 import BusinessStack from "../components/BusinessStack";
 
 export default function Business() {
+  const [businesses, setBusinesses] = useState([]);
+
+  useEffect(() => {
+    const fetchBusinesses = () => {
+      axios
+        .get("/api/businesses")
+        .then((response) => {
+          setBusinesses(response.data);
+          console.log("Fetched businesses:" + response.data);
+        })
+        .catch((error) => {
+          console.error("Error fetching businesses:", error);
+        });
+    };
+
+    fetchBusinesses();
+  }, []);
+
   return (
     <div className='BusiLis px-10'>
       <div className='flex justify-end mr-10 mt-8'>
@@ -10,47 +30,17 @@ export default function Business() {
           <Link to='/business/new'>Add Business</Link>
         </button>
       </div>
-      {/* TODO: get business list from backend including id */}
-      <table className='table-auto w-full'>
-        <thead>
-          <tr className='-mx-3'>
-            <th>
-              <BusinessStack
-                name='Business Name'
-                content='Lorem Ipsum Dolor Sit'
-                img='https://tinyurl.com/mrem4k68'
-                id='1'
-              />
-            </th>
-            <th>
-              <BusinessStack
-                name='Business Name'
-                content='Lorem Ipsum Dolor Sit'
-                img='https://tinyurl.com/mrem4k68'
-                id='2'
-              />
-            </th>
-          </tr>
-          <tr className='-mx-3'>
-            <th>
-              <BusinessStack
-                name='Business Name'
-                content='Lorem Ipsum Dolor Sit'
-                img='https://tinyurl.com/mrem4k68'
-                id='3'
-              />
-            </th>
-            <th>
-              <BusinessStack
-                name='Business Name'
-                content='Lorem Ipsum Dolor Sit'
-                img='https://tinyurl.com/mrem4k68'
-                id='4'
-              />
-            </th>
-          </tr>
-        </thead>
-      </table>
+      <div>
+        <h1 className='text-title'>Businesses</h1>
+        {businesses.map((business) => (
+          <BusinessStack
+            key={business.id}
+            id={business.id}
+            name={business.name}
+            content={business.content}
+          />
+        ))}
+      </div>
     </div>
   );
 }
